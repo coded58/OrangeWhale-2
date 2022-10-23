981a8c1abf924283c3400c0fca47b8f7e7067cf4
BasicWebTechnology <basicwebtech@gmail.com>
2022-10-23 04:57:25 +0100

    initial commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,23 @@
 import "./styles.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../../pages/Dashboard";
+import Activity from "../../pages/Activity";
+import Drafts from "../../pages/Drafts";
+import Settings from "../../pages/Settings";
+import Post from "../../pages/Post";
+import Published from "../../pages/Published";
+import Scheduled from "../../pages/Scheduled";
 
 const Main = () => {
   return (
     <div className="main">
-      <h1>Main</h1>
+      <Dashboard />
+      <Activity />
+      <Settings />
+      <Post />
+      <Published />
+      <Scheduled />
+      <Drafts />
     </div>
   );
 };
