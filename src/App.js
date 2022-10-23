5110bbd6ec24c980a3a786bff7fb8a1aa6237ea9
BasicWebTechnology <basicwebtech@gmail.com>
2022-10-23 04:34:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Left from "./components/Left";
+import { Left, Main } from "./components";
 
 const App = () => {
   return (
     <div className="App">
       <Left />
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>Main</h1>
+    </div>
+  );
+};
+
+export default Main;
