8cbf943c7402ed3b9b2c9633ec2bfb2a11980adb
BasicWebTechnology <basicwebtech@gmail.com>
2022-10-23 05:10:11 +0100

    initial commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import Card from "../reusableComponents/card/Card";
+import Target from "../reusableComponents/target/Target";
+import Table from "../reusableComponents/table/Table";
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="main-content">
+        <span className="title">May 2022 Performance</span>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+      <div className="main-content">
+        <span className="title">Statistics</span>
+        <div className="cards">
+          <Target />
+        </div>
+      </div>
+      <div className="main-content">
+        <span className="title">Monthly Tasks</span>
+        <div className="table-container">
+          <Table />
+        </div>
+      </div>
     </div>
   );
 };
